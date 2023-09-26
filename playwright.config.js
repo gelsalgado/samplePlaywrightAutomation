2c9dfa637d89a18fab6d215bd651cad23b76ae79
Angelito Salgado <angelito.rhdjapan@gmail.com>
2023-09-26 17:59:32 +0900

    Update readme file and set parallel runs to false since the browsers sometime crash on my system.

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Increase test timeout */
-  timeout: 120000,
+  timeout: 180000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
